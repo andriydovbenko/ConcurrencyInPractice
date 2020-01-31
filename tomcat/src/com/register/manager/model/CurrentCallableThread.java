6e9ac4da04f9721d71724e84174408af77225300
Andrii Dovbenko <andriydovbenko1@gmail.com>
2020-01-31 12:24:56 +0100

    added model CurrentCallableThread to receive response from AccounRecorder

---
+++ tomcat/src/com/register/manager/model/CurrentCallableThread.java
@@ -0,0 +1,19 @@
+package com.register.manager.model;
+
+import com.register.manager.service.AccountRecorder;
+
+import java.util.concurrent.Callable;
+
+public class CurrentCallableThread implements Callable<String> {
+    private AccountRecorder accountRecorder = AccountRecorder.getInstance();
+    private int accountIndex;
+
+    public CurrentCallableThread(int accountIndex) {
+        this.accountIndex = accountIndex;
+    }
+
+    @Override
+    public String call() throws Exception {
+        return accountRecorder.getAccountInformation(accountIndex);
+    }
+}
