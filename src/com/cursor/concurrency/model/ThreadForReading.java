3fa32e0a861bcf596f0a261cdd067e456dbf3edf
Andrii Dovbenko <andriydovbenko1@gmail.com>
2020-02-10 15:22:53 +0100

    made randomValue instance of class ThreadForReading

--- src/com/cursor/concurrency/model/ThreadForReading.java
+++ src/com/cursor/concurrency/model/ThreadForReading.java
@@ -8,11 +8,11 @@ public class ThreadForReading implements Runnable {
     private static final int ITERATION_VALUE = 10;
     private static final int RANGE_OF_VALUE = 400_000;
     private DataBase db = DataBase.getInstance();
+    private static  int randomValue = new Random().nextInt(RANGE_OF_VALUE);
 
     @Override
     public void run() {
         for (int i = 1; i < ITERATION_VALUE; i++) {
-            int randomValue = new Random().nextInt(RANGE_OF_VALUE);
             System.out.println("Operation number " + i + ". Reading from Data Base.");
             if (db.getNumberIfPresetOrNull(randomValue) == null) {
                 System.out.println("Number " + randomValue + " doesn't exist");
